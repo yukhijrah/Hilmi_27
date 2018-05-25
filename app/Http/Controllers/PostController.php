9c54834fdbaa0cb5804a4b13482529f11ca7a7c3
Fadhil_HLmy <fadhilcuevas27@gmail.com>
2018-05-25 14:34:19 +0700

    Buat Method Show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,8 +70,9 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        return view('posts.show');
+    {   
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
     }
 
     /**
